52af1a6ebeeb0b6dbf587b31f307378ac9458f46
Dzikri Aziz <kvcrvt@gmail.com>
2019-11-28 11:23:44 +0800

    Update plugin description

--- plugin.php
+++ plugin.php
@@ -2,6 +2,7 @@
 /**
  * Plugin Name: Utility Taxonomy
  * Plugin URI: https://humanmade.com
+ * Description: A hidden taxonomy, used for filtering of posts/pages etc. in a way that is more performant than using the likes of post meta.
  * Author: Human Made
  * Author URI: https://humanmade.com
  */
