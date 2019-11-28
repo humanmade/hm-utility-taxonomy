3355f644a66d76d502e8a0b07b156381333cbb3e
Dzikri Aziz <kvcrvt@gmail.com>
2019-11-28 12:57:32 +0800

    Update terms fo real

--- assets/src/components/Panel.js
+++ assets/src/components/Panel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { compose } from '@wordpress/compose';
 import { CheckboxControl, ToggleControl } from '@wordpress/components';
@@ -15,33 +15,45 @@ export function Panel( props ) {
 		id,
 		// multiple, // TODO.
 		options,
+		postTerms,
 		title,
+		taxObject,
 		taxTerms,
+		updateTerms,
 	} = props;
 
 	if ( ! taxTerms || ! taxTerms.length ) {
 		return null;
 	}
 
-	const finalOptions = taxTerms.map( ( { slug } ) => {
-		return options.find( item => item.value === slug );
+	const finalOptions = taxTerms.map( ( { id, slug } ) => {
+		const option = options.find( item => item.value === slug );
+
+		if ( ! option ) {
+			return null;
+		}
+
+		return {
+			...option,
+			value: id,
+		};
+
 	} ).filter( Boolean );
 
 	if ( ! finalOptions.length ) {
 		return null;
 	}
 
-	const [ selected, select ]= useState( defaultValue );
 	const Control = options.length > 1
 		? CheckboxControl
 		: ToggleControl;
 
 	const onChange = ( checked, value ) => {
-		const nextSelected = checked
-			? selected.concat( value )
-			: selected.filter( item => item !== value );
+		const nextPostTerms = checked
+			? postTerms.concat( value )
+			: postTerms.filter( item => item !== value );
 
-		select( nextSelected );
+		updateTerms( nextPostTerms, taxObject.rest_base );
 	};
 
 	return (
@@ -49,7 +61,7 @@ export function Panel( props ) {
 			{ finalOptions.map( ( { label, value } ) => (
 				<Control
 					key={ `${ className }-${ id }-${ value }` }
-					checked={ selected.indexOf( value ) >= 0 }
+					checked={ postTerms.indexOf( value ) >= 0 }
 					label={ label }
 					onChange={ checked => onChange( checked, value ) }
 				/>
@@ -79,7 +91,7 @@ Panel.propTypes = {
 	title: PropTypes.string.isRequired,
 	// Props below are supplied by `addSelectors()`.
 	hasAssignAction: PropTypes.bool.isRequired,
-	postTerms: PropTypes.array.isRequired, // TODO.
+	postTerms: PropTypes.arrayOf( PropTypes.number ).isRequired,
 	taxObject: PropTypes.shape( {
 		name: PropTypes.string.isRequired,
 		rest_base: PropTypes.string.isRequired,
