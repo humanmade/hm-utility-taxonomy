fb29fc51175920908c1d0deff4f1db045000d8b3
Dzikri Aziz <kvcrvt@gmail.com>
2019-11-28 16:35:18 +0800

    Do heavy lifting in dispatchers

--- assets/src/components/Panel/dispatchers.js
+++ assets/src/components/Panel/dispatchers.js
@@ -1,9 +1,17 @@
-export default function addDispatchers( dispatch ) {
+export default function addDispatchers( dispatch, ownProps ) {
 	const { editPost } = dispatch( 'core/editor' );
+	const { getPostTerms, taxonomy } = ownProps;
+	const postTerms = getPostTerms();
+
+	const updateTerms = ( checked, termId ) => {
+		const nextPostTerms = checked
+			? postTerms.concat( termId )
+			: postTerms.filter( item => item !== termId );
+
+		editPost( { [ taxonomy ]: nextPostTerms } );
+	};
 
 	return {
-		updateTerms( terms, taxonomy ) {
-			editPost( { [ taxonomy ]: terms } );
-		},
+		updateTerms,
 	}
 }
