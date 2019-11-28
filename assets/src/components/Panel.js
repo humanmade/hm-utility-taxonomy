dc13de1a7f7f3c2e44f55ee6c29eae1d32158057
Dzikri Aziz <kvcrvt@gmail.com>
2019-11-28 12:44:15 +0800

    Move panel selectors to its own file

--- assets/src/components/Panel.js
+++ assets/src/components/Panel.js
@@ -5,6 +5,8 @@ import { compose } from '@wordpress/compose';
 import { CheckboxControl, ToggleControl } from '@wordpress/components';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 
+import panelSelectors from '../data/panel-selectors';
+
 export function Panel( props ) {
 	const {
 		className,
@@ -89,38 +91,8 @@ Panel.propTypes = {
 	} ) ),
 };
 
-export function addSelectors( select, ownProps ) {
-	const { options, taxonomy } = ownProps;
-	const { getCurrentPost, getEditedPostAttribute } = select( 'core/editor' );
-	const { getEntityRecords, getTaxonomy } = select( 'core' );
-	const { _links: postLinks } = getCurrentPost();
-	const taxObject = getTaxonomy( taxonomy );
-
-	// Taxonomy object hasn't been fetched yet.
-	if ( ! taxObject ) {
-		return {
-			taxObject,
-			hasAssignAction: false,
-			postTerms: [],
-			taxTerms: [],
-		};
-	}
-
-	const { rest_base: restBase } = taxObject;
-	const termSlugs = options.map( ( { value } ) => value );
-
-	return {
-		taxObject,
-		hasAssignAction: 'wp:action-assign-' + restBase in postLinks,
-		postTerms: getEditedPostAttribute( restBase ),
-		taxTerms: getEntityRecords( 'taxonomy', restBase, {
-			slug: termSlugs,
-		} ),
-	};
-}
-
 const ComposedPanel = compose( [
-	withSelect( addSelectors ),
+	withSelect( panelSelectors ),
 ] )( Panel );
 
 export default ComposedPanel;
---
+++ assets/src/data/panel-selectors.js
@@ -0,0 +1,30 @@
+export default function panelSelectors( select, ownProps ) {
+	const { options, taxonomy } = ownProps;
+	const { getCurrentPost, getEditedPostAttribute } = select( 'core/editor' );
+	const { getEntityRecords, getTaxonomy } = select( 'core' );
+	const { _links: postLinks } = getCurrentPost();
+	const taxObject = getTaxonomy( taxonomy );
+
+	// Taxonomy object hasn't been fetched yet.
+	if ( ! taxObject ) {
+		return {
+			taxObject,
+			hasAssignAction: false,
+			postTerms: [],
+			taxTerms: [],
+		};
+	}
+
+	const { rest_base: restBase } = taxObject;
+	const termSlugs = options.map( ( { value } ) => value );
+
+	return {
+		taxObject,
+		hasAssignAction: 'wp:action-assign-' + restBase in postLinks,
+		postTerms: getEditedPostAttribute( restBase ),
+		taxTerms: getEntityRecords( 'taxonomy', restBase, {
+			slug: termSlugs,
+		} ),
+	};
+}
+
