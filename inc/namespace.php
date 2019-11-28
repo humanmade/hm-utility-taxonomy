419ec5a10f6cc7297e87a4ecb27519816c6dfb9f
Dzikri Aziz <kvcrvt@gmail.com>
2019-11-28 12:02:22 +0800

    Use default rest_base

--- inc/namespace.php
+++ inc/namespace.php
@@ -44,7 +44,6 @@ function register_tax() : void {
 		$post_types,
 		[
 			'public'       => false,
-			'rest_base'    => 'hm-utilities',
 			'rewrite'      => false,
 			'show_in_rest' => true,
 			'labels'       => [
