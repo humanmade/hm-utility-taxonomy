ca7a36ee8a21d4fc088a931f975b7e31af6e844e
Dzikri Aziz <dzikri.aziz@gmail.com>
2020-01-07 12:21:12 +0800

    Panel selectors: Pass isCleanNewPost to Panel

--- assets/src/components/Panel/selectors.js
+++ assets/src/components/Panel/selectors.js
@@ -1,11 +1,12 @@
 export default function addSelectors( select, ownProps ) {
 	const { taxonomy } = ownProps;
-	const { getCurrentPost, getEditedPostAttribute } = select( 'core/editor' );
+	const { getCurrentPost, getEditedPostAttribute, isCleanNewPost } = select( 'core/editor' );
 	const { _links: postLinks } = getCurrentPost();
 	const getPostTerms = () => getEditedPostAttribute( taxonomy );
 
 	return {
 		getPostTerms,
 		hasAssignAction: 'wp:action-assign-' + taxonomy in postLinks,
+		isCleanNewPost,
 	};
 }
