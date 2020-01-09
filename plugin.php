2be2ec8d25b7b32e8bcfced62813bed626610fa0
Dzikri Aziz <dzikri@humanmade.com>
2020-01-09 13:55:56 +0800

    Bump version -> 1.1.1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: A hidden taxonomy, used for filtering of posts/pages etc. in a way that is more performant than using the likes of post meta.
  * Author: Human Made
  * Author URI: https://humanmade.com
- * Version: 1.1.0
+ * Version: 1.1.1
  */
 
 namespace HM\Utility_Taxonomy;
